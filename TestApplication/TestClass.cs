e1c07d90ef97b74c608e085acf03880fda77c362
sumanmitra17 <suman.mitra@indusnet.co.in>
2017-12-14 15:30:32 +0530

    First Commit

---
+++ TestApplication/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestApplication
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver dr = new ChromeDriver(@"E:\Suman\Softwares\Cell\Visual Studio\Driver");
+            dr.Url = "https://www.google.com/";
+            String s = dr.Title;
+
+            Console.WriteLine(s);
+        }
+    }
+}
